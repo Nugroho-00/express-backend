21265f6210f2329c2cb532540787f16b61bea06c
Nugroho-00 <snugroho211@gmail.com>
2020-09-30 12:57:19 +0700

    change user model config

--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,58 @@ const db = require('../helpers/db')
 const table = 'user'
 
 module.exports = {
-  getIdUserModel: (id, cb) => {
+  getDetailUserModel: (id, cb) => {
     db.query(`SELECT * FROM ${table} WHERE id= ${id}`, (_err, result, field) => {
       cb(result)
     })
   },
-  getAllUserModel: (searchKey, searchValue, sortColumn, sortValue, limit, offset, cb) => {
-    db.query(`SELECT * FROM ${table} WHERE ${searchKey} LIKE '%${searchValue}%' ORDER BY ${sortColumn} ${sortValue} LIMIT ${limit} OFFSET ${offset}`, (err, result, field) => {
-      cb(err, result)
+  getUserModel: (data = []) => {
+    return new Promise((resolve, reject) => {
+      db.query(`SELECT * FROM ${table} LIMIT ? OFFSET ?`, data, (err, result, _fields) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
     })
   },
-  userModel: (arr, cb) => {
-    db.query(`SELECT COUNT(*) AS count FROM ${table} WHERE ${arr[0]} LIKE '%${arr[1]}%'`, (_err, data, field) => {
-      cb(data)
+  countUserModel: () => {
+    return new Promise((resolve, reject) => {
+      db.query(`SELECT COUNT(*) AS count FROM ${table}`, (err, result, _fields) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result[0].count)
+        }
+      })
     })
   },
-  createUserModel: (arr, cb) => {
-    db.query(`INSERT INTO ${table} (userName, email, password) VALUES ('${arr[0]}', '${arr[1]}', '${arr[2]}')`, (err, result, field) => {
-      cb(err, result)
+  getUserByCondition: (data = {}) => {
+    return new Promise((resolve, reject) => {
+      db.query(`SELECT * FROM ${table} WHERE ?`, data, (err, result, _fields) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
+    })
+  },
+  // userCondition: (arr, cb) => {
+  //   db.query(`SELECT * FROM ${table} WHERE '${arr[0]}' AND '${arr[1]}'`, (err, data, _fields) => {
+  //     cb(err, data)
+  //   })
+  // },
+  createUserModel: (data = {}) => {
+    return new Promise((resolve, reject) => {
+      db.query(`INSERT INTO ${table} SET ?`, data, (err, result, _fields) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
     })
   },
   updatePartialUserModel: (id, data, cb) => {
