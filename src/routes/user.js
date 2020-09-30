fa4f00b2be11110225b84ec172bd3aa3fa64abc0
Nugroho-00 <snugroho211@gmail.com>
2020-09-30 12:56:37 +0700

    change for user add routes upload & authLogin

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,10 +3,15 @@ const { createUser, getUserId, getAllUser, updatePartialUser, deleteUser } = req
 
 const router = Router()
 
+// import middleware auth
+//  const authMiddleware = require('../middleware/auth')
+
+const uploadHelper = require('../helpers/upload')
+
 router.get('/:id', getUserId)
 router.get('/', getAllUser)
-router.post('/', createUser)
-router.patch('/:id', updatePartialUser)
+router.post('/', uploadHelper.single('picture'), createUser)
+router.patch('/:id', uploadHelper.single('picture'), updatePartialUser)
 router.delete('/:id', deleteUser)
 
 module.exports = router
