77748de978d548dc02b9744b2cfe7cbe7137c02d
Nugroho-00 <snugroho211@gmail.com>
2020-09-30 12:42:52 +0700

    add upload/file multer

---
+++ src/helpers/upload.js
@@ -0,0 +1,18 @@
+const multer = require('multer')
+
+// const options = {
+//   dest: 'assets/uploads/'
+// }
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'assets/uploads/')
+  },
+  filename: (req, file, cb) => {
+    const ext = file.originalname.split('.')[file.originalname.split('.').length - 1]
+    const fileName = new Date().getTime().toString().concat('.').concat(ext)
+    cb(null, fileName)
+  }
+})
+
+module.exports = multer({ storage, limit: { flieSize: 1000000 } })
