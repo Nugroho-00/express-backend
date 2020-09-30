680c2dbf5faf9b77777ec68ebe50534f5cb4f9d4
Nugroho-00 <snugroho211@gmail.com>
2020-09-30 12:55:59 +0700

    change for item routes

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,13 +1,15 @@
 const { Router } = require('express')
-const { getDetailItem, createItem, getItems, updateItem, updatePartialItem, deleteItem } = require('../controllers/items')
+const { getItemId, createItem, getItems, updateItem, updatePartialItem, deleteItem } = require('../controllers/items')
 
 const router = Router()
 
+const uploadHelper = require('../helpers/upload')
+
 router.get('/', getItems)
-router.get('/:id', getDetailItem)
-router.post('/', createItem)
+router.get('/:id', getItemId)
+router.post('/', uploadHelper.single('picture'), createItem)
 router.put('/:id', updateItem)
-router.patch('/:id', updatePartialItem)
+router.patch('/:id', uploadHelper.single('picture'), updatePartialItem)
 router.delete('/:id', deleteItem)
 
 module.exports = router
