0293b935987730012acca78ebe566c46793decbe
Nugroho-00 <snugroho211@gmail.com>
2020-09-30 12:58:55 +0700

    change chart model config

--- src/models/chart.js
+++ src/models/chart.js
@@ -5,21 +5,21 @@ const table3 = 'item'
 
 module.exports = {
   createChartModel: (arr, cb) => {
-    db.query(`INSERT INTO ${table1} (userID, itemsID) VALUES (${arr[0]}, ${arr[1]})`, (err, result, field) => {
+    db.query(`INSERT INTO ${table1} (userId, itemId, quantity, colorId) VALUES (${arr[0]}, ${arr[1]}, ${arr[2]}, ${arr[3]})`, (err, result, field) => {
       cb(err, result)
     })
   },
   getChartModel: (arr, limit, offset, cb) => {
-    db.query(`SELECT ${table1}.id, ${table2}.userName, ${table3}.name, ${table3}.price, ${table1}.createdAt FROM ${table1} 
+    db.query(`SELECT ${table1}.id, ${table2}.userName, ${table3}.name, ${table1}.quantity, ${table3}.price, ${table1}.createdAt FROM ${table1} 
     INNER JOIN ${table2} ON ${table1}.userID = ${table2}.id
     INNER JOIN ${table3} ON ${table1}.itemsID = ${table3}.id
-    WHERE ${arr[0]} LIKE '%${arr[1]}%' ORDER BY ${arr[2]} ${arr[3]} LIMIT ${limit} OFFSET ${offset}`, (_err, result, field) => {
-      cb(_err, result)
+    WHERE ${arr[0]} LIKE '%${arr[1]}%' ORDER BY ${arr[2]} ${arr[3]} LIMIT ${limit} OFFSET ${offset}`, (err, result, field) => {
+      cb(err, result)
     })
   },
   ChartModel: (arr, cb) => {
-    db.query(`SELECT COUNT(*) AS count FROM ${table1} WHERE ${arr[0]} LIKE '%${arr[1]}%'`, (_err, result, field) => {
-      cb(result)
+    db.query(`SELECT COUNT(*) AS count FROM ${table1} WHERE ${arr[0]} LIKE '%${arr[1]}%'`, (_err, data, field) => {
+      cb(data)
     })
   },
   getChartIdModel: (id, cb) => {
