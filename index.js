f2d10267f244d9194959ba56de6391026d30f26e
Nugroho-00 <snugroho211@gmail.com>
2020-10-08 08:59:02 +0700

    fix for app.use index.js

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
+require('dotenv').config()
+const { APP_PORT } = process.env
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-require('dotenv').config()
-
 const app = express()
-
-const itemsRouter = require('./src/routes/items')
-const categoryRouter = require('./src/routes/kategori')
-const chartRouter = require('./src/routes/chart')
-const profileRouter = require('./src/routes/profile')
-const userRouter = require('./src/routes/user')
-const roleUserRouter = require('./src/routes/roleUser')
-const authRouter = require('./src/routes/auth')
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 
-// import middleware auth
-const authMiddleware = require('./src/middleware/auth')
+// import middleware
+const {
+  authAdmin, authSeller, authCust
+} = require('./src/middleware/auth')
 
-// import provide static file
-app.use('/uploads', express.static('assets/uploads'))
+// provide static files
+app.use('/uploads', express.static('assets/uploads/'))
 
-// routes
-app.use('/items', itemsRouter)
-app.use('/category', categoryRouter)
-app.use('/chart', chartRouter)
+// import route
+// const homeRouter = require('./src/routes/public/homeRoutes')
+const authRouter = require('./src/routes/authPublic')
+const adminRouter = require('./src/routes/admin')
+const sellerRouter = require('./src/routes/seller')
+const custRouter = require('./src/routes/customer')
+
+// app.use('/home', homeRouter)
 app.use('/auth', authRouter)
-// app.use('/user', userRouter)
-app.use('/profile', profileRouter)
-app.use('/roles', roleUserRouter)
-app.use('/user', authMiddleware, userRouter)
+app.use('/admin', authAdmin, adminRouter)
+app.use('/seller', authSeller, sellerRouter)
+app.use('/customer', authCust, custRouter)
 
-app.listen(8000, () => {
-  console.log('App listening on port http://localhost:8000')
+app.listen(APP_PORT, () => {
+  console.log(`App listening on port ${APP_PORT}`)
 })
